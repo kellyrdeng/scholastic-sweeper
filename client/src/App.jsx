cbec377b0d6c75732d92bf96da91d099b31d031b
jiexy <ifechumek@gmail.com>
2023-11-11 17:27:47 -0500

    grid setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,11 @@
 import './App.css'
 
 function App() {
-  return <h1 className='bg-red-200'>HI: TESTING</h1>
+  return (
+    <div className='h-screen w-full'>
+
+    </div>
+  )
 }
 
 export default App
---
+++ client/src/components/grid.jsx
@@ -0,0 +1,5 @@
+export default function grid() {
+  return (
+    <div>grid</div>
+  )
+}
