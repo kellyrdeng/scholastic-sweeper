79eda855ef6e82a95a40b5b63c73398355a06efd
jiexy <ifechumek@gmail.com>
2023-11-11 23:10:02 -0500

    basic grid styles

--- client/src/assets/gameState.js
+++ client/src/assets/gameState.js
@@ -1,7 +1,8 @@
 const gameState = {
     MINE: -1,
     FLAG: -2,
-    BLANK: -3
+    BLANK: -3,
+    COLORS: ["transparent", "rgb(55 48 163)", "rgb(22 101 52)", "rgb(153 27 27)"]
 }
 
 
--- client/src/components/Cell.jsx
+++ client/src/components/Cell.jsx
@@ -1,16 +1,53 @@
 import React from 'react'
 import baseCell from "../assets/baseCell.svg";
 import axios from "axios"
+import gameState from '../assets/gameState';
 
-export default function Cell({value}) {
+const testGrid = [
+    [-3, -3, 1, 0, 0, 0, 0, 0, 0],
+    [-3, -3, 3, 1, 1, 0, 0, 0, 0],
+    [-3, -3, 3, -1, 1, 0, 1, 1, 1],
+    [-2, 2, 2, 1, 2, 1, 2, -2, 1],
+    [ 2, 2, 0, 0, 1, -2, 2, 1, 1],
+    [-2, 1, 0, 1, 2, 2, 1, 0, 0],
+    [ 1, 1, 0, 1, -1, 1, 1, 1, 1],
+    [ 0, 0, 0, 1, 2, 2, 2, -1, 1],
+    [ 0, 0, 0, 0, 1, -2, 2, 1, 1]
+]
+
+export default function Cell({pos}) {
+    const [rowIdx, colIdx] = pos
+    const value = testGrid[rowIdx][colIdx]
+    let cell
+    switch(value){
+        case gameState.BLANK:
+            cell = <img className="" src={baseCell} />
+            break
+        case gameState.FLAG:
+            cell = <img className="" src={baseCell} />
+            break
+        case gameState.MINE:
+            cell = <img className="" src={baseCell} />
+            break
+        default:
+            cell = (
+                <div className='h-14 flex w-14 bg-transparent items-center justify-center text-red-700 border-[1px] border-zinc-400'>
+                    <h1 className='font-mono font-extrabold text-4xl'
+                    style={{color: gameState.COLORS[value]}}>{value}</h1>
+                </div>
+                    )
+    }
+    
     const handleClick = async (event) => {
+        console.log(pos)
        await axios.post("http://localhost:3000/", {
         message: "I was clicked"
     })
     }
+
   return (
-    <div className='relative' onClick={handleClick}>
-        <img className="" src={baseCell} />
+    <div className='relative h-14 w-14' onClick={handleClick}>
+        {cell}
     </div>
   )
 }
