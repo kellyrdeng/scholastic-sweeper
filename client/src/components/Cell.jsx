12aa146a102040ae8c59fbd9f99d1728e11ab1bb
Kelly Deng <dengkellyr@gmail.com>
2023-11-11 23:11:35 -0500

    space lol

--- client/src/components/Cell.jsx
+++ client/src/components/Cell.jsx
@@ -6,7 +6,7 @@ export default function Cell({value}) {
     const handleClick = async (event) => {
        await axios.post("http://localhost:3000/", {
         message: "I was clicked"
-    })
+      })
     }
   return (
     <div className='relative' onClick={handleClick}>
