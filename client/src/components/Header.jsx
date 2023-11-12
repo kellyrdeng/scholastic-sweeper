0ca95d12af65bb29a32fcedcd241831b2f5682a1
AliciaPeace <you@example.com>
2023-11-12 07:53:40 -0500

    z-index added, keeps it from clipping into grid

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import logo from '../assets/imgs/logo.png'
 export default function Header() {
   return (
-    <div id="main_header" className="bg-stone-500 w-screen h-24 sticky top-0">
+    <div id="main_header" className="bg-stone-500 w-screen h-24 sticky top-0 z-50">
         <nav id="nav-set">
             <a class="no-underline p-2 w-80 h-auto" href="/">
               <img id="logo"className="" src={logo}/>
