0d5cd395fae5ed15e3cb3be925090b5271f35f23
AliciaPeace <you@example.com>
2023-11-12 08:56:57 -0500

    added more body to the home page

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -15,6 +15,14 @@ export default function Home() {
 				As for the playable grid it is as it sounds! A generated 9x9 Beginner
 				grid with ten mines, with which you have the ability to <span class="em">flag, chord,</span> and <span class="em">sweep!</span> 
 				</p>
+				<h1 className="pt-8 content-center font-serif"><b>Overview</b></h1>
+				<p className ="font-mono leading-relaxed">
+				According to <em>Minesweeper Online</em> the rules of Minesweeper as as follows... The board is 
+				divided into cells, with mines randomly distributed. To win, you need to open all the cells.
+				The number on a cell shows the number of mines adjacent to it. Using this information,
+				you can determine cells that are safe, and cells that contain mines. Cells suspected of
+				being mines can be marked with a flag using the right mouse button. 
+				</p>
 			</main>
 
 		</div>
