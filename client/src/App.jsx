ab6f265cc835a2d92064cf5ef6ee8ded9a84f2a6
AliciaPeace <you@example.com>
2023-11-11 20:03:31 -0500

    change of name

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Grid from "./components/grid";
+import Grid from "./components/Grid";
 
 export default function App() {
 	return (
