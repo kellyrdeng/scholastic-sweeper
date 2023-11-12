96dbc2861166280aea0680b8474ce81b6aaf989d
AliciaPeace <you@example.com>
2023-11-12 03:30:19 -0500

    About page done, error regardin icon header remain

--- client/src/About.jsx
+++ client/src/About.jsx
@@ -2,18 +2,18 @@ import React from "react";
 
 export default function About() {
 	return (
-		<div className="">
-			<main className="bg-white h-full">
-				<h1 className="content-center font-serif"><em>Welcome to ScholasticSweeper!</em></h1>
-				<p className ="font-mono ">
+		<div>
+			<main id="About_Main"className="m-auto m-0" >
+				<h1 className="pt-8 content-center font-serif"><b>Welcome to ScholasticSweeper!</b></h1>
+				<p className ="font-mono leading-relaxed">
 				We are ScholasticSweeper, a student-created website that provides 
 				tutorials on how to play Minesweeper and a playable grid that you 
-				can sweep to your heart's content! The tutorial portion of ScholasticSweeper
+				can sweep to your heart's content The tutorial portion of ScholasticSweeper
 				is a randomly generated Beginner grid (9x9 with ten mines) that is cleared
 				with step by step explanations of the logic behind each move. 
-				These explanations cover patterns, chording, and efficiency as warranted.
-				As for the playable grid it is as it sounds! A randomly generated 9x9 Beginner
-				grid with ten mines, with which you have the ability to flag, chord, and sweep! 
+				These explanations cover <span class="em">patterns, chording,</span> and <span id="about_span">efficiency</span> as warranted.
+				As for the playable grid it is as it sounds! A generated 9x9 Beginner
+				grid with ten mines, with which you have the ability to <span class="em">flag, chord,</span> and <span class="em">sweep!</span> 
 				</p>
 			</main>
 
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,9 +3,9 @@ import logo from '../assets/imgs/logo.png'
 export default function Header() {
   return (
     <div id="main_header" className="bg-stone-500 w-screen h-24">
-        <nav id = "nav-set">
-            <a class="no-underline w-64 h-full" href="/">
-              <img className="h-full w-full" src={logo} />
+        <nav id="nav-set">
+            <a class="no-underline w-64 h-auto" href="/">
+              <img id="logo" className="pt-8 ps-10" src={logo}/>
             </a>
             <a class="font-mono ps-20 p-8 text-4xl" href="/about">About</a>
             <a class="font-mono ps-20 p-8 text-4xl" href="/play">Play</a> 
