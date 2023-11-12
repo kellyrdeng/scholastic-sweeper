b41669c7dd807faf6f9118ba57531d0ed4c50f62
Kelly Deng <dengkellyr@gmail.com>
2023-11-12 00:51:01 -0500

    minor changes didn't do much

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ export default function App() {
 					<Route path="/about" element={<About />} />
 					<Route path="/grid" element={<MineGrid />} />
 					<Route path="/tutorial" element={<Tutorial />} />
+					<Route path="/play" element={<Play />} />
 				</Routes>
 			</BrowserRouter>
 		</div>
