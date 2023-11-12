5d72be317b829e36486aa73ce50b3f8f402ea1a5
jiexy <ifechumek@gmail.com>
2023-11-12 01:31:01 -0500

    navigation setup

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import logo from '../assets/imgs/logo.png'
 export default function Header() {
   return (
     <div id="main_header" className="bg-stone-500 w-screen h-24">
         <nav id = "nav-set">
-          <Link to="/tutorial">
-          <img src={logo}/>
-          </Link>
-            <a class="font-mono ps-20 p-8 text-4xl" href="">About</a>
-            <a class="font-mono ps-20 p-8 text-4xl" href="">Play</a> 
-            <a class="font-mono ps-20 p-8 text-4xl" href="">Tutorial</a>
+            <a class="no-underline w-64 h-full" href="/">
+              <img className="h-full w-full" src={logo} />
+            </a>
+            <a class="font-mono ps-20 p-8 text-4xl" href="/about">About</a>
+            <a class="font-mono ps-20 p-8 text-4xl" href="/play">Play</a> 
+            <a class="font-mono ps-20 p-8 text-4xl" href="/tutorial">Tutorial</a>
         </nav>   
     
     </div>
