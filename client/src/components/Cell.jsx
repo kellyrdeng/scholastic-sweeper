0d6a6a0e8e3ec28447cb7f467f22916de25c2466
jiexy <ifechumek@gmail.com>
2023-11-12 04:00:07 -0500

    tutorials completed

--- client/src/components/Cell.jsx
+++ client/src/components/Cell.jsx
@@ -4,74 +4,66 @@ import flag from "../assets/flag.svg";
 import axios from "axios";
 import gameState from "../assets/gameState";
 
-const testGrid = [
-  [-3, -3, 1, 0, 0, 0, 0, 0, 0],
-  [-3, -3, 3, 1, 1, 0, 0, 0, 0],
-  [-3, -3, 3, -1, 1, 0, 1, 1, 1],
-  [-2, 2, 2, 1, 2, 1, 2, -2, 1],
-  [2, 2, 0, 0, 1, -2, 2, 1, 1],
-  [-2, 1, 0, 1, 2, 2, 1, 0, 0],
-  [1, 1, 0, 1, -1, 1, 1, 1, 1],
-  [0, 0, 0, 1, 2, 2, 2, -1, 1],
-  [0, 0, 0, 0, 1, -2, 2, 1, 1],
-];
+export default function Cell({ grid, pos, tutorialCell }) {
+	const activeStyle =
+		tutorialCell && tutorialCell[0] === pos[0] && tutorialCell[1] === pos[1]
+			? " border-yellow-500 border-4 border-box rounded-sm"
+			: "";
+	const [rowIdx, colIdx] = pos;
+	const value = grid[rowIdx][colIdx];
+	let cell;
+	switch (value) {
+		case gameState.BLANK:
+			cell = <img className={"" + activeStyle} src={baseCell} />;
+			break;
+		case gameState.FLAG:
+			cell = (
+				<div className={"h-full w-full relative flex items-center justify-center" + activeStyle}>
+					<img className="absolute" src={baseCell} />;
+					<img className="absolute h-8 w-full z-10" src={flag} />;
+				</div>
+			);
+			break;
+		case gameState.MINE:
+			cell = <img className={"" + activeStyle} src={baseCell} />;
+			break;
+		default:
+			cell = (
+				<div className={"h-14 flex w-14 bg-transparent items-center justify-center text-red-700 border-[1px] border-zinc-400" + activeStyle}>
+					<h1
+						className="font-mono font-extrabold text-4xl"
+						style={{ color: gameState.COLORS[value] }}
+					>
+						{value}
+					</h1>
+				</div>
+			);
+	}
 
-export default function Cell({ pos }) {
-  const [rowIdx, colIdx] = pos;
-  const value = testGrid[rowIdx][colIdx];
-  let cell;
-  switch (value) {
-    case gameState.BLANK:
-      cell = <img className="" src={baseCell} />;
-      break;
-    case gameState.FLAG:
-      cell = (
-        <div className="h-full w-full relative flex items-center justify-center">
-          <img className="absolute" src={baseCell} />;
-          <img className="absolute h-8 w-full z-10" src={flag} />;
-        </div>
-      );
-      break;
-    case gameState.MINE:
-      cell = <img className="" src={baseCell} />;
-      break;
-    default:
-      cell = (
-        <div className="h-14 flex w-14 bg-transparent items-center justify-center text-red-700 border-[1px] border-zinc-400">
-          <h1
-            className="font-mono font-extrabold text-4xl"
-            style={{ color: gameState.COLORS[value] }}
-          >
-            {value}
-          </h1>
-        </div>
-      );
-  }
+	const handleClick = async (event) => {
+		if (event.button === 0) {
+			//left button
+			await axios.post("http://localhost:3000/", {
+				gameState: [],
+				answerGrid: [],
+				coords: [],
+				action: "leftclick",
+			});
+		}
+		if (event.button === 2) {
+			//right button
+			await axios.post("http://localhost:3000/", {
+				gameState: [],
+				answerGrid: [],
+				coords: [],
+				action: "rightclick",
+			});
+		}
+	};
 
-  const handleClick = async (event) => {
-    if (event.button === 0) {
-      //left button
-      await axios.post("http://localhost:3000/", {
-        gameState: [],
-        answerGrid: [],
-        coords: [],
-        action: "leftclick",
-      });
-    }
-    if (event.button === 2) {
-      //right button
-      await axios.post("http://localhost:3000/", {
-        gameState: [],
-        answerGrid: [],
-        coords: [],
-        action: "rightclick",
-      });
-    }
-  };
-
-  return (
-    <div className="relative h-14 w-14" onClick={handleClick}>
-      {cell}
-    </div>
-  );
+	return (
+		<div className={"relative h-14 w-14"} onClick={handleClick}>
+			{cell}
+		</div>
+	);
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import logo from '../assets/imgs/logo.png'
 export default function Header() {
   return (
-    <div id="main_header" className="bg-stone-500 w-screen h-24">
+    <div id="main_header" className="bg-stone-500 w-screen h-24 fixed top-0">
         <nav id = "nav-set">
             <a class="no-underline w-64 h-full" href="/">
               <img className="h-full w-full" src={logo} />
